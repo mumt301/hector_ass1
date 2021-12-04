4400da8a64b7c73e599b55a173d80b1bec4631fb
Teyssier <hector.teyssier@mail.mcgill.ca>
2021-12-04 17:38:08 -0500

    Added second IR o play with.

--- convolver.js
+++ convolver.js
@@ -5,10 +5,17 @@ function runAfterLoadingPage(){
 
     // We create a convolver
     var convolver = new Pizzicato.Effects.Convolver({
-        impulse: './media/final_proj/small_church_IR.wav',
+        impulse: './media/final_proj/quiet_street_IR.wav',
         mix: 0.5
     }, function() {
         console.log('Convolver ready to be used.');
+    });
+
+    var convolver2 = new Pizzicato.Effects.Convolver({
+        impulse: './media/final_proj/St_Andrews_Sydney_IR.wav',
+        mix: 0.5
+    }, function() {
+        console.log('Convolver 2 ready to be used.');
     });
     
     // We create a sound, that corresponds to the input microphone
@@ -32,7 +39,26 @@ function runAfterLoadingPage(){
 
     convolution_amount.addEventListener("change", function() {
         convolver.mix = (convolution_amount.value)/100;
-        console.log("Value of slider: ", convolution_amount.value);
+        document.getElementById("amount").innerHTML = "Intensity : " +convolution_amount.value+"%";
+    }, false);
+
+
+    const play_buton2 = document.getElementById("playBtn2");
+    const stop_button2 = document.getElementById("stopBtn2");
+    const convolution_amount2 = document.getElementById("rangeInput2");
+
+    play_buton2.addEventListener("click", function () {
+        voice.addEffect(convolver2);
+    });
+
+    stop_button2.addEventListener("click", function () {
+        voice.removeEffect(convolver2);
+    });
+
+    convolution_amount2.addEventListener("change", function() {
+        convolver2.mix = (convolution_amount2.value)/100;
+        // console.log("Value of slider: ", convolution_amount2.value);
+        document.getElementById("amount2").innerHTML =  "Intensity : " +convolution2_amount.value+"%";
     }, false);
 
 }
